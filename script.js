5009a481770b8013ac47dd8ff5a91ec7d317b19c
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-04-23 18:36:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Speed the Playback up
+// Increase the the Playback speed of the video.
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.demo-video').playbackRate = 3.0;
